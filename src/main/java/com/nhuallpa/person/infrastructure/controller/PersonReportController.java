320173ae99a2a5ad63fe40895025a44fb5bc6fdd
nestor.huallpa <huallpa.nestor@gmail.com>
2024-10-27 13:47:47 -0300

    SENV-1537 remove unused imports

--- src/main/java/com/nhuallpa/person/infrastructure/controller/PersonReportController.java
+++ src/main/java/com/nhuallpa/person/infrastructure/controller/PersonReportController.java
@@ -6,7 +6,6 @@ import com.nhuallpa.person.infrastructure.factory.ReportPresenterFactory;
 import com.nhuallpa.person.infrastructure.response.ReportReponse;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
